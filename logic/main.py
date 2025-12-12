290c04ae90b1c750be3c2d3bebb7bb193ad0898a
kazact <kazact95@gmail.com>
2025-12-12 14:40:24 -0600

    Adds module docstring and moves TODO comments to it

--- logic/main.py
+++ logic/main.py
@@ -1,16 +1,21 @@
-"""This will be a program to rename MAME save states"""
+"""MAMEStates core logic
+
+This module encompasses the static functions used by the MAMEStates application.
+
+TODO:
+    * Add docstrings to each function.
+    * Consider renaming functions to improve human readability.
+"""
 import subprocess
 import os
 
 
-# TODO More descriptive comment
 # Get list of rom names
 def create_rom_list(mame_path: str) -> None:
     with open("logic/romlist.txt", "w") as romlist:
         subprocess.run([mame_path + '\\mame.exe', "-ll"], stdout=romlist)
 
 
-# TODO Comment
 def build_rom_db(romlist: str) -> dict[str, str]:
     rom_db = {}
     with open(romlist, 'r') as romlist:
@@ -32,14 +37,12 @@ def get_roms_with_saves(mame_path: str) -> list[str]:
     return contents
 
 
-# TODO Update Comment
 # For save folder in list, get real name
 def get_real_name(rom_db: dict[str, str], rom_name: str) -> str:
     real_name = rom_db[rom_name]
     return real_name
 
 
-# TODO Update Comment
 # For save folder in list, get save file names
 def get_save_names(games_with_saves: list[str], mame_folder: str) -> dict[str, list[str]]:
     save_states = {}
@@ -55,13 +58,11 @@ def get_save_names(games_with_saves: list[str], mame_folder: str) -> dict[str, l
     return save_states
 
 
-# TODO Comment
 def rename(mame_folder: str, rom_folder: str, old_save_name: str, new_save_name: str) -> None:
     os.rename(mame_folder + "\\sta\\" + rom_folder + "\\" + old_save_name + '.sta',
               mame_folder + "\\sta\\" + rom_folder + "\\" + new_save_name + '.sta')
 
 
-# TODO Rename to be more descriptive.
 def change_mame_path(new_path: str) -> None:
     with open('logic/romlist.txt', 'r') as romlist:
         data = romlist.read().splitlines(True)
