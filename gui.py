aa6eca49a8412f73d0032d797da2c9249b0edf6f
kazact <kazact95@gmail.com>
2025-12-13 19:17:57 -0600

    Add MainWindow class docstring

--- gui.py
+++ gui.py
@@ -22,21 +22,15 @@ from logic.main import get_roms_with_saves, get_save_names, get_real_name, build
 from logic.main import change_mame_path
 
 
-# TODO Comment
 # Subclass so I can alter event handling behavior
 class TreeWidget(QTreeWidget):
     """Subclasses and extends the QTreeWidget class of the PyQt6.QtWidgets Module
 
     This class extends the keyPressEvent method for the purposes of capturing a custom key press
 
-    Attributes:
-        self.mame_folder: Some description
-        self.description_db: Description
-        self.rom_db: Description
-
     """
     def __init__(self, mame_folder: str, description_db: dict[str, str], rom_db: dict[str, str]):
-        """Initiate the TreeWidget Subclass
+        """Initialize the TreeWidget Subclass
 
         The TreeWidget subclass inherits most of its behavior from its parent class QTreeWidget.
         TreeWidget initializes with all the data needed by its single extended method
@@ -97,10 +91,24 @@ class TreeWidget(QTreeWidget):
 # TODO Comment
 # TODO Clean up init
 class MainWindow(QMainWindow):
+    """Subclasses and extends the QQMainWindow class of the PyQt6.QtWidgets Module
 
+    This class inherits most of its behavior from its parent class, while extending its functionality.
+    Houses all GUI elements of the MAMEStates application.
+
+    """
     def __init__(self):
+        """Initialize the MainWindow subclass
+
+        The MainWindow subclass inherits most of its behavior from, and extends, its parent class QMainWindow. The
+        initialization process setups the GUI elements present on program launch, as well as connecting signals to
+        slots. In addition, the initialization process also creates persistent files required by the MAMEStates
+        application.
+
+        """
         super().__init__()
 
+        # TODO Consider if this should be a function for the purposes of clarity, testing, ect.
         if os.path.isfile('logic/romlist.txt'):
             with open('logic/romlist.txt', 'r') as romlist:
                 first_line = romlist.readline()
@@ -141,7 +149,6 @@ class MainWindow(QMainWindow):
         # Widget customization
         self.setWindowTitle('MAME States')
         self.tree_widget = TreeWidget(self.mame_folder, self.description_db, self.rom_db)
-        print(self.tree_widget.__doc__)
         self.tree_widget.setHeaderLabels(['Games'])
 
         # Signals
@@ -168,11 +175,13 @@ class MainWindow(QMainWindow):
 
     # Slots
     def update_treewidget(self) -> None:
+        """Update the data structures the TreeWidget derives its items from."""
         self.tree_widget.rom_db = self.rom_db
         self.tree_widget.description_db = self.description_db
         self.tree_widget.mame_folder = self.mame_folder
 
     def add_sub_items(self) -> None:
+        """Add a sub item to a top level item of the TreeWidget."""
         # Adds save states as sub items to a given games main item.
         pprint.pprint(self.game_items)
         for game in self.game_items:
@@ -183,6 +192,7 @@ class MainWindow(QMainWindow):
                         QTreeWidgetItem(game, [state])
 
     def add_top_level_items(self) -> None:
+        """Create, and capture a reference to, top level items in the TreeWidget."""
         # Creates top level list items. Also captures reference to every item created in a list.
         # These references will be used to create sub items.
         for game in self.real_names:
@@ -190,6 +200,7 @@ class MainWindow(QMainWindow):
             self.game_items.append(game_item)
 
     def fill_data_structures(self) -> None:
+        """Reset and refill data structures used to derive TreeWidget items."""
         # reset data structs
         self.rom_db = build_rom_db('logic/romlist.txt')
         self.real_names = []
@@ -206,6 +217,7 @@ class MainWindow(QMainWindow):
         self.saves = get_save_names(roms_with_saves, self.mame_folder)
 
     def menu_button_clicked(self) -> None:
+        """Change active MAME directory and reload TreeWidget."""
         mame_path = QFileDialog.getExistingDirectory(self, 'Choose a Directory',
                                                      options=QFileDialog.Option.ShowDirsOnly)
         self.mame_folder = mame_path
@@ -224,6 +236,7 @@ class MainWindow(QMainWindow):
     #         pass
 
     def item_double_clicked(self, item: QTreeWidgetItem, col: int) -> None:
+        """Open text editor on  a subitem of TreeWidget."""
         if item.parent() is not None:
             self.text_before_editing = item.text(0)
             self.tree_widget.openPersistentEditor(item, col)
@@ -236,6 +249,7 @@ class MainWindow(QMainWindow):
     # On program load, prev is always None, and cur is first item.
     # TODO Clean this up and remove text prompts.
     def selection_changed(self, cur: QTreeWidgetItem, prev: QTreeWidgetItem) -> None:
+        """Revert text change on previously selected subitem of TreeWidget."""
         if prev and cur:
             if prev.parent():
                 prev.setText(0, self.text_before_editing)
