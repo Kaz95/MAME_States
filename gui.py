3e9a38638d806ddc9560a51f7aba8b27c749a588
kazact <kazact95@gmail.com>
2026-01-11 16:56:05 -0600

    Enter to close split editors

--- gui.py
+++ gui.py
@@ -58,6 +58,7 @@ class StageSplitItem(QWidget):
 
         self.name_editor.editingFinished.connect(self.update_split_db)
         self.score_editor.editingFinished.connect(self.update_split_db)
+        self.name_editor.editingFinished.connect(self.toggle_labels)
 
         self.score_editor.setValidator(QIntValidator())
         # self.label: QLabel = QLabel(f'Stage-{stage}:')
