b920370f918fedd7364fa8ee067aff3f287d459e
kazact <kazact95@gmail.com>
2026-01-12 08:37:25 -0600

    Fix 'can't access high score editor' bug.

--- gui.py
+++ gui.py
@@ -58,7 +58,7 @@ class StageSplitItem(QWidget):
 
         self.name_editor.editingFinished.connect(self.update_split_db)
         self.score_editor.editingFinished.connect(self.update_split_db)
-        self.name_editor.editingFinished.connect(self.toggle_labels)
+        self.name_editor.returnPressed.connect(self.toggle_labels)
 
         self.score_editor.setValidator(QIntValidator())
         # self.label: QLabel = QLabel(f'Stage-{stage}:')
