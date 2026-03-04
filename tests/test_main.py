91640e707509cda1d2381626300b423b7c4bf844
kazact <kazact95@gmail.com>
2026-03-04 07:32:07 -0600

    Update tests to use monkeypatch/mock objects.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
 
-from logic.main import resource_path
+from logic.main import resource_path, get_roms_with_saves
 from pathlib import Path
 import sys
+from logic import main
+
 
 
 
@@ -12,12 +14,23 @@ def test_resource_path_parent_dir_with_str_input():
 def test_resource_path_parent_dir_with_path_input():
     assert resource_path(Path('abc')) == Path(f'{Path(__file__).parent.parent}/abc')
 
-def test_resource_path_pyinstaller_archive_with_str_input():
-    sys._MEIPASS = r'S:\ome\base\path'
+def test_resource_path_pyinstaller_archive_with_str_input(monkeypatch):
+    monkeypatch.setattr(sys, '_MEIPASS', 'S:/ome/base/path', raising=False)
     assert resource_path('abc') == Path('S:/ome/base/path/abc')
-    delattr(sys, '_MEIPASS')
 
-def test_resource_path_pyinstaller_archive_with_path_input():
-    sys._MEIPASS = Path(r'S:\ome\base\path')
+
+def test_resource_path_pyinstaller_archive_with_path_input(monkeypatch):
+    monkeypatch.setattr(sys, '_MEIPASS', 'S:/ome/base/path', raising=False)
     assert resource_path('abc') == Path('S:/ome/base/path/abc')
-    delattr(sys, '_MEIPASS')
+
+
+def test_get_roms_with_saves(mocker):
+    mock_files = ['file1.sta', 'file2.sta', 'file3.sta']
+    mocker.patch('logic.main.os.listdir', return_value=mock_files)
+    some_dir = Path('abc')
+
+    assert get_roms_with_saves(some_dir) == mock_files
+    main.os.listdir.assert_called_once_with(some_dir / 'sta')
+
+def test_get_all_input_files():
+    pass
