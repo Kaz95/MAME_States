8eea67366433c5ea26a9b6dd7a0a30bbfec38e3e
kazact <kazact95@gmail.com>
2026-03-05 08:10:54 -0600

    Optimize imports.

--- gui.py
+++ gui.py
@@ -26,8 +26,7 @@ from logic.core import delete_split
 from logic.core import rom_description_from_name, delete_personal_best, delete_splits, \
     save_pb_to_database, save_pbs, has_xml, get_new_pb, \
     prepare_pb_for_db, PersonalBests, get_mame_version, MAMEDir, \
-    save_mame_dirs, get_formatted_rom_info, \
-    get_personal_bests, Split, PersonalBest, resource_path
+    save_mame_dirs, Split, PersonalBest, resource_path
 
 
 class MainWindow(QMainWindow):
