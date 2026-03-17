e1fcef3fec753607cd5bf664aac22bd63edd11bd
kazact <kazact95@gmail.com>
2026-03-17 06:04:29 -0500

    Remove try/except where program should be allowed to fail.

--- mamestates/hi2txt_wrapper.py
+++ mamestates/hi2txt_wrapper.py
@@ -52,13 +52,10 @@ def get_hs_tables(hi2txt_compatible_hi_scores: dict[str, list[Path]]) -> dict[st
         hiscore_files = hi2txt_compatible_hi_scores[mame_dir]
         for file in hiscore_files:
             print(f'Score is: {file}')
-            try:
-                results = subprocess.run([core.get_abs_path(r'.\hi2txt\hi2txt.exe'), '-r', f'{file}'],
-                                         cwd=core.get_abs_path(r'.\hi2txt'), capture_output=True, text=True,
-                                         check=True, encoding='utf-8')
-                hi2txt_tables[mame_dir][f'{file.stem}'] = results.stdout
-            except FileNotFoundError:
-                print('whoops')
+            results = subprocess.run([core.get_abs_path(r'.\hi2txt\hi2txt.exe'), '-r', f'{file}'],
+                                     cwd=core.get_abs_path(r'.\hi2txt'), capture_output=True, text=True,
+                                     check=True, encoding='utf-8')
+            hi2txt_tables[mame_dir][f'{file.stem}'] = results.stdout
     return hi2txt_tables
 
 
