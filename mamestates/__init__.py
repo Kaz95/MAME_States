f19d05fefb607eba003aa9b19b6a4dc45339e2c4
kazact <kazact95@gmail.com>
2026-04-05 13:38:31 -0500

    Update TODO.

--- mamestates/__init__.py
+++ mamestates/__init__.py
@@ -7,7 +7,6 @@ TODO:
     * Testing.
     * Set default MAME directory of some kind. Possibly, base it on current monitor to avoid GroovyMAME jank.
         * DoubleClick rom to launch in default MAME.
-    * Disallow stage splits with blank name. Also, disallow duplicate names.
     * Add program default location.
     * Derive roms dir from MAME ini, instead of assuming it is called roms.
     * Remove QMessageBpx when dir deleted via file menu.
