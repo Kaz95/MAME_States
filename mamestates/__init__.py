71b0ab997dc6c0c27560de48050403b0b1c584dd
kazact <kazact95@gmail.com>
2026-05-14 17:35:33 -0500

    Update TODO

--- mamestates/__init__.py
+++ mamestates/__init__.py
@@ -2,13 +2,9 @@
 
 TODO:
     * Read, and update all docstrings.
-    * Programmatically change focus where appropriate. Raise and focus program where appropriate.
-    * Add GUI 'updates' where appropriate(eg., new item added, personal bests updated)
     * Testing.
     * Set default MAME directory of some kind. Possibly, base it on current monitor to avoid GroovyMAME jank.
         * DoubleClick rom to launch in default MAME.
-    * Add program default location.
-    * Derive roms dir from MAME ini, instead of assuming it is called roms.
     * Consider sizing policies and size hints
     * Decide on new features to add.
 """
